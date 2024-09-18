893efc1c3e141fb422e8a34cb251719ec7e7989d
Samuel Suzan <fullcandies@gmail.com>
2024-09-18 16:08:57 +0200

    Create object class and fix buffer bug

--- ASCII-Dash-Project/Objects/Object.cpp
+++ ASCII-Dash-Project/Objects/Object.cpp
@@ -2,11 +2,30 @@
 
 Object::Object()
 {
-	//_objectSprite[0][0].Char.AsciiChar = 'D' ;
+	Init();
 }
 
-std::vector<std::vector<CHAR_INFO>> Object::GetObjectSprite()
+Object::~Object()
+{
+	delete _objectSprite;
+}
+
+CHAR_INFO* Object::GetObjectSprite()
 {
 	return _objectSprite;
 }
 
+void Object::Init()
+{
+	_sizeSprite.X = 20;
+	_sizeSprite.Y = 20;
+	_objectSprite = new CHAR_INFO[_sizeSprite.X * _sizeSprite.X];
+	for(int i = 0; i < _sizeSprite.X; i ++)
+	{
+		for(int j=0; j< _sizeSprite.Y; j++)
+		{
+			_objectSprite[i + (j * (_sizeSprite.X - 1))].Char.AsciiChar = '\0';
+		}
+	}
+	_objectSprite[0].Char.AsciiChar = 'H';
+}
